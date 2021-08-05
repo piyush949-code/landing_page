b44577f4c6d3ba28304a11b5c60da071be962bc7
Piyush Mali <piyush.m.hashcodejr@gmail.com>
2021-08-05 10:46:06 +0530

    updated project

---
+++ src/components/gallery.jsx
@@ -0,0 +1,15 @@
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Projects</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+             </div>
+    </div>
+  )
+}
